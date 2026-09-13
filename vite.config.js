fcec8d86796a067024a5fcfe48ad6ad42b9cc1bb
Abdur Rahman <abdurrahmanaipy@gmail.com>
2026-09-13 14:29:12 +0430

    add Github Actions

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // Standard Vite config with the React plugin (enables JSX + Fast Refresh).
 export default defineConfig({
+  baseUrl: "/Cost-Intilegent/",
   plugins: [react()],
 });
